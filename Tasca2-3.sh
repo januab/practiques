010ad50b975f1d65c58e9553d26a0d350bfcec0d
Jan <jan@email.cat>
2023-11-23 15:45:06 +0100

    Tasca 2-3 feta

---
+++ Tasca2-3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+num_pelis_pagina=7
+
+f_entrada=$1
+ratinglevel=$2
+cadena=$3
+
+nova_cerca="si"
+
+while [ $nova_cerca == "si" ]
+do
+	grep ^[^,]*$cadena[^,]*,[^,]*$ratinglevel[^,]* $f_entrada | sort -t , -k1 > pelis_ordenades_tmp.csv
+
+	num_linies=`wc -l < pelis_ordenades_tmp.csv`
+
+	if [ $num_linies -eq 0 ]; then
+		echo "";
+		echo "Pel·lícula no trobada"
+	else
+		clear
+		for(( i=1; i<=$num_linies; i++ ))
+		do
+			linia=`head -$i pelis_ordenades_tmp.csv | tail -1`
+			titol=`echo $linia | cut -d"," -f1`
+			ratinglevel=`echo $linia | cut -d"," -f2`
+			ratingdescription=`echo $linia | cut -d"," -f3`
+			release_year=`echo $linia | cut -d"," -f4`
+			user_rating_score=`echo $linia | cut -d"," -f5`
+			user_rating_size=`echo $linia | cut -d"," -f6`
+
+			echo "*****************************************************************************"
+			echo "*Títol: $titol"
+			echo "*Any: $release_year *Nivell de classificació: $ratinglevel"
+			echo "*Descripció: $ratingdescription"
+			echo "*Valoració dels usuaris: $user_rating_score *Mida de la mostra: $user_rating_size"
+
+			let resta=$i%$num_pelis_pagina
+
+			if [ $resta -eq 6 ];then
+				echo " ";
+				echo "Prem enter per continuar o s per sortir:"
+				read -n1 var
+				clear
+				
+				if [ ${#var} -eq 1 ]; then
+					if [ $var == "s" ];then
+						break
+					fi
+				fi	
+			fi
+		done 
+	fi
+	echo "";
+	echo "Vols fer una altra cerca? [si/no]:"
+	read nova_cerca
+	if [ $nova_cerca == si ];then
+		clear
+		echo "Introdueix un Rating Level:"
+		read ratinglevel
+		echo "Introdueix una cadena:"
+		read cadena
+	fi
+done
+
+rm pelis_ordenades_tmp.csv
